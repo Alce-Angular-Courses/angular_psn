d5c9f2706f1ad4bdbc7ef0c83976e3a0e54fea3e
alce65 <alce65@hotmail.es>
2018-10-15 17:56:24 +0200

    Ajustes iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-psn';
+  title = 'Angular PSN';
+  logo = '../assets/logo.svg';
 }
