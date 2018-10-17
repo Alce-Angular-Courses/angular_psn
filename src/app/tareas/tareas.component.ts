2da3512c17ce6eab189cccf05772a9568fd23d4d
alce65 <alce65@hotmail.es>
2018-10-17 18:10:17 +0200

    Modulos funcionales

---
+++ src/app/tareas/tareas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'psn-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
