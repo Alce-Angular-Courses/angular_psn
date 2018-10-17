4f153938ad33eae78c6df44950ecdbbf5cbb9c9e
alce65 <alce65@hotmail.es>
2018-10-17 18:28:23 +0200

    Componente saludo

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SaludoComponent } from './saludo/saludo.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, SaludoComponent]
 })
 export class HomeModule { }
