18818e653cdaf76d3ad015ffcd1385a1b5077d1b
alce65 <alce65@hotmail.es>
2018-10-24 17:06:50 +0200

    Validacion formularios

--- src/app/contactos/form-td/form-td.component.ts
+++ src/app/contactos/form-td/form-td.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ContactoIf, Contacto } from 'src/app/models/contacto.modelo';
 
 @Component({
@@ -8,11 +8,22 @@ import { ContactoIf, Contacto } from 'src/app/models/contacto.modelo';
 })
 export class FormTdComponent implements OnInit {
 
+  @ViewChild('cForm') cForm: any;
+  aCursos: Array<any>;
   contacto: ContactoIf;
   constructor() { }
 
   ngOnInit() {
     this.contacto = new Contacto();
+    this.aCursos = [
+      { nombre: 'Angular', codigo: 'ng001'},
+      { nombre: 'React', codigo: 're001'},
+      { nombre: 'NodeJS', codigo: 'nd001'}];
   }
 
+  onSubmit(ev) {
+    ev.preventDefault();
+    console.log('Enviado');
+    console.log(this.cForm);
+  }
 }
